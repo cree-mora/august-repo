314afb602bf0909834a94de066c145b081940766
cree-mora <creestine231@gmail.com>
2023-08-16 02:44:37 -0700

    demo

---
+++ lasser/demo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num1=5,num2=32,num3=45
+	printf("Enter the three numbers:");
+	scannf("%d,%d,%d,"&5,&32,&45);
+	if (5<32)
+	{
+		if(32<45)
+		{
+			printf("largest number is:%d,"45);
+		}
+		else if (32>45)
+		{
+			prinf("largest number is:%d,"32);
+		}
+		else
+		{
+			printf("largest number is:%d,"5);
+		}
+		return 0;
+	}
